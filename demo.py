c688416f916446f4e047d2994a32e9207f017bb6
5456465tony97202 <109021079@live.asia.edu.tw>
2021-04-09 15:38:58 +0800

    用函式進行整理

--- demo.py
+++ demo.py
@@ -18,28 +18,37 @@ def get_resource(url):
 def parse_html(html_str):
     return BeautifulSoup(html_str,"lxml")
 
-if __name__=="__main__":
-   urls=generate_urls(URL,1,10) #求的資料1~n+1
-   for url in urls:
+def get_word(soup,file):
+    words=[]
+    count=0 #計算自數(可省)
+    for wordlist_table in soup.find_all(class_="wordlist"):
+         count+=1
+         for word_entry in wordlist_table.find_all("tr"):
+             new_word = []
+             new_word.append(file)
+             new_word.append(str(count)) #計算自數(可省)
+             new_word.append(word_entry.th.text)
+             new_word.append(word_entry.td.text)
+             words.append(new_word)
+    return words       
+
+def web_scraping_bot(urls):
+    eng_words=[]
+    for url in urls:
        file=url.split("/") [-1] #spilt 切割 [-1]求html檔名最後一個
        print("catching:", file,"web data...") 
        r=get_resource(url)
        if r.status_code == requests.codes.ok:
-           soup=parse_html(r.text)
-           words=[] 
-           count=0 #計算自數(可省)
-           for wordlist_table in soup.find_all(class_="wordlist"):
-               count+=1
-               for word_entry in wordlist_table.find_all("tr"):
-                   new_word = []
-                   new_word.append(file)
-                   new_word.append(str(count)) #計算自數(可省)
-                   new_word.append(word_entry.th.text)
-                   new_word.append(word_entry.td.text)
-                   words.append(new_word)
-           print(words)
-           print("------------\n\n")
+          soup=parse_html(r.text)
+          words=get_word(soup,file)
+          eng_words=eng_words + words
+          print("waiting 5 second...")
+          time.sleep(5)
        else:
            print("HTTP request error!!")
-       print("sleep 5 second")
-       time.sleep(5)
+    return eng_words
+
+if __name__=="__main__":
+   urls=generate_urls(URL,1,10) #求的資料1~n+1
+   eng_words=web_scraping_bot(urls)
+   print(eng_words)
